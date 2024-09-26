4314a7c64c3cce113b00b6968bdcbe259e814318
gruelas <gruelasjr@gmail.com>
2024-09-26 12:32:59 -0600

    Update ResponseHelper.php
    Improved documentation and added an Exception Handler Method that provides a response based on the error code

--- src/Helpers/ResponseHelper.php
+++ src/Helpers/ResponseHelper.php
@@ -1,7 +1,15 @@
 <?php
 
+/**
+ * @author Gabriel Ruelas
+ * @license MIT
+ * @version 1.0.0
+ *
+ */
+
 namespace Equidna\Toolkit\Helpers;
 
+use Exception;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Response;
 
@@ -16,10 +24,15 @@ class ResponseHelper
     }
 
     /**
-     * Returns a bad request response.
-     * @param string $message The error message.
-     * @param string|null $forward_url The URL to redirect to.
-     * @return Response|RedirectResponse The response object.
+     * Generates a response for a bad request.
+     *
+     * This method determines the type of response based on whether the request is an API or hook request.
+     * If it is, it returns a 400 response with the provided message. Otherwise, it either redirects back
+     * with an error message or to a specified URL with an error message.
+     *
+     * @param string $message The error message to be included in the response.
+     * @param string|null $forward_url The URL to redirect to if the request is not an API or hook request. Defaults to null.
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse A 400 response or a redirect response with an error message.
      */
     public static function badRequest(string $message, string $forward_url = null): Response|RedirectResponse
     {
@@ -34,10 +47,21 @@ class ResponseHelper
         return redirect($forward_url)->withErrors([$message])->withInput();
     }
 
+    /**
+     * Generates a response for an unauthorized request.
+     *
+     * This method determines the type of response based on whether the request is an API or hook request.
+     * If it is, it returns a 401 response with the provided message. Otherwise, it either redirects to the login URL
+     * with an error message or to a specified URL with an error message.
+     *
+     * @param string $message The error message to be included in the response.
+     * @param string|null $forward_url The URL to redirect to if the request is not an API or hook request. Defaults to null.
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse A 401 response or a redirect response with an error message.
+     */
     public static function unautorized(string $message, string $forward_url = null): Response|RedirectResponse
     {
         if (RouteHelper::isAPI() || RouteHelper::isHook()) {
-            return response($message, 403);
+            return response($message, 401);
         }
 
         if (is_null($forward_url)) {
@@ -47,6 +71,17 @@ class ResponseHelper
         return redirect($forward_url)->withErrors([$message])->withInput();
     }
 
+    /**
+     * Generates a response for a forbidden request.
+     *
+     * This method determines the type of response based on whether the request is an API or hook request.
+     * If it is, it returns a 403 response with the provided message. Otherwise, it either redirects back
+     * with an error message or to a specified URL with an error message.
+     *
+     * @param string $message The error message to be included in the response.
+     * @param string|null $forward_url The URL to redirect to if the request is not an API or hook request. Defaults to null.
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse A 403 response or a redirect response with an error message.
+     */
     public static function forbidden(string $message, string $forward_url = null): Response|RedirectResponse
     {
         if (RouteHelper::isAPI() || RouteHelper::isHook()) {
@@ -60,6 +95,17 @@ class ResponseHelper
         return redirect($forward_url)->withErrors([$message])->withInput();
     }
 
+    /**
+     * Generates a response for a not found request.
+     *
+     * This method determines the type of response based on whether the request is an API or hook request.
+     * If it is, it returns a 404 response with the provided message. Otherwise, it either redirects back
+     * with an error message or to a specified URL with an error message.
+     *
+     * @param string $message The error message to be included in the response.
+     * @param string|null $forward_url The URL to redirect to if the request is not an API or hook request. Defaults to null.
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse A 404 response or a redirect response with an error message.
+     */
     public static function notFound(string $message, string $forward_url = null): Response|RedirectResponse
     {
         if (RouteHelper::isAPI() || RouteHelper::isHook()) {
@@ -73,6 +119,17 @@ class ResponseHelper
         return redirect($forward_url)->withErrors([$message])->withInput();
     }
 
+    /**
+     * Generates a response for an internal server error.
+     *
+     * This method determines the type of response based on whether the request is an API or hook request.
+     * If it is, it returns a 500 response with the provided message. Otherwise, it either redirects back
+     * with an error message or to a specified URL with an error message.
+     *
+     * @param string $message The error message to be included in the response.
+     * @param string|null $forward_url The URL to redirect to if the request is not an API or hook request. Defaults to null.
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse A 500 response or a redirect response with an error message.
+     */
     public static function error(string $message, string $forward_url = null): Response|RedirectResponse
     {
         if (RouteHelper::isAPI() || RouteHelper::isHook()) {
@@ -84,5 +141,30 @@ class ResponseHelper
         }
 
         return redirect($forward_url)->withErrors([$message])->withInput();
+    }
+
+    /**
+     * Handles exceptions and returns an appropriate response based on the exception code.
+     *
+     * @param Exception $exception The exception to handle.
+     * @param string|null $forward_url Optional URL to forward to in case of an error.
+     * @return Response|RedirectResponse The response corresponding to the exception code.
+     */
+    public static function handleException(Exception $exception, string $forward_url = null): Response|RedirectResponse
+    {
+        switch ($exception->getCode()) {
+            case 400:
+                return self::badRequest($exception->getMessage(), $forward_url);
+            case 401:
+                return self::unautorized($exception->getMessage(), $forward_url);
+            case 403:
+                return self::forbidden($exception->getMessage(), $forward_url);
+            case 404:
+                return self::notFound($exception->getMessage(), $forward_url);
+            case 500:
+                return self::error($exception->getMessage(), $forward_url);
+            default:
+                return self::error('An unexpected error occurred. (' . $exception->getCode() . '-' . $exception->getMessage() . ')', $forward_url);
+        }
     }
 }
