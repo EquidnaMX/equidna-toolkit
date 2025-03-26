aa431699c2603a71120759e29fef0c0a61acc06b
gruelasjr <gruelasjr@gmail.com>
2025-03-26 16:32:30 -0600

    Update ResponseHelper.php

--- src/Helpers/ResponseHelper.php
+++ src/Helpers/ResponseHelper.php
@@ -67,7 +67,7 @@ class ResponseHelper
      * @param string $message The message to include in the response.
      * @param array $errors An array of errors to include in the response. Default is an empty array.
      * @param string|null $forward_url Optional URL to redirect to.
-     * @return string|Response|RedirectResponse The generated response.
+     * @return mixed The generated response.
      */
     public static function badRequest(string $message, array $errors = [], ?string $forward_url = null): string|JsonResponse|RedirectResponse
     {
