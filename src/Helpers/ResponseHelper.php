5a60b1ff80c4f8f5263cbc5ee94c330cab51d41d
gruelasjr <gruelasjr@gmail.com>
2025-03-26 16:11:39 -0600

    Update ResponseHelper.php

--- src/Helpers/ResponseHelper.php
+++ src/Helpers/ResponseHelper.php
@@ -29,11 +29,12 @@ class ResponseHelper
      *
      * @param int $status The HTTP status code of the operation.
      * @param string $message The message to be included in the response.
+     * @param array $errors An array of errors to include in the response. Default is an empty array.
      * @param string|null $forward_url The URL to redirect to if applicable. Default is null.
      *
      * @return mixed
      */
-    private static function generateResponse(int $status, string $message, ?string $forward_url): mixed
+    private static function generateResponse(int $status, string $message, array $errors = [], ?string $forward_url = null): string|JsonResponse|RedirectResponse
     {
         if (RouteHelper::isConsole()) {
             return $message;
@@ -41,8 +42,9 @@ class ResponseHelper
 
         if (RouteHelper::isAPI() || RouteHelper::isHook()) {
             $response = [
+                'code'    => $status,
                 'message' => $message,
-                'code'    => $status
+                'errors'  => $errors
             ];
 
             return response()
@@ -63,84 +65,108 @@ class ResponseHelper
      * Generates a 400 Bad Request response.
      *
      * @param string $message The message to include in the response.
+     * @param array $errors An array of errors to include in the response. Default is an empty array.
      * @param string|null $forward_url Optional URL to redirect to.
      * @return string|Response|RedirectResponse The generated response.
      */
-    public static function badRequest(string $message, ?string $forward_url = null): string|JsonResponse|RedirectResponse
+    public static function badRequest(string $message, array $errors = [], ?string $forward_url = null): string|JsonResponse|RedirectResponse
     {
-        return self::generateResponse(400, $message, $forward_url);
+        return self::generateResponse(
+            status: 400,
+            message: $message,
+            errors: $errors,
+            forward_url: $forward_url
+        );
     }
 
     /**
      * Generates a 401 Unauthorized response.
      *
      * @param string $message The message to include in the response.
+     * @param array $errors An array of errors to include in the response. Default is an empty array.
      * @param string|null $forward_url The URL to forward to, if any.
      * @return string|Response|RedirectResponse The generated response.
      */
-    public static function unautorized(string $message, ?string $forward_url = null): string|JsonResponse|RedirectResponse
+    public static function unautorized(string $message, array $errors = [], ?string $forward_url = null): string|JsonResponse|RedirectResponse
     {
-        return self::generateResponse(401, $message, $forward_url);
+        return self::generateResponse(
+            status: 401,
+            message: $message,
+            errors: $errors,
+            forward_url: $forward_url
+        );
     }
 
     /**
      * Generates a 403 Forbidden response.
      *
      * @param string $message The message to include in the response.
+     * @param array $errors An array of errors to include in the response. Default is an empty array.
      * @param string|null $forward_url Optional URL to redirect to.
      * @return string|Response|RedirectResponse The generated response.
      */
-    public static function forbidden(string $message, ?string $forward_url = null): string|JsonResponse|RedirectResponse
+    public static function forbidden(string $message, array $errors = [], ?string $forward_url = null): string|JsonResponse|RedirectResponse
     {
-        return self::generateResponse(403, $message, $forward_url);
+        return self::generateResponse(
+            status: 403,
+            message: $message,
+            errors: $errors,
+            forward_url: $forward_url
+        );
     }
 
     /**
      * Generates a 404 Not Found response.
      *
      * @param string $message The message to include in the response.
+     * @param array $errors An array of errors to include in the response. Default is an empty array.
      * @param string|null $forward_url Optional URL to redirect to.
      * @return string|Response|RedirectResponse The generated response.
      */
-    public static function notFound(string $message, ?string $forward_url = null): string|JsonResponse|RedirectResponse
+    public static function notFound(string $message, array $errors = [], ?string $forward_url = null): string|JsonResponse|RedirectResponse
     {
-        return self::generateResponse(404, $message, $forward_url);
+        return self::generateResponse(
+            status: 404,
+            message: $message,
+            errors: $errors,
+            forward_url: $forward_url
+        );
     }
 
     /**
      * Generates a 409 Conflict response.
      *
      * @param string $message The message to include in the response.
+     * @param array $errors An array of errors to include in the response. Default is an empty array.
      * @param string|null $forward_url Optional URL to redirect to.
      * @return string|Response|RedirectResponse The generated response.
      */
-    public static function conflict(string $message, ?string $forward_url = null): string|JsonResponse|RedirectResponse
+    public static function conflict(string $message, array $errors = [], ?string $forward_url = null): string|JsonResponse|RedirectResponse
     {
-        return self::generateResponse(409, $message, $forward_url);
+        return self::generateResponse(
+            status: 409,
+            message: $message,
+            errors: $errors,
+            forward_url: $forward_url
+        );
     }
-
-    /**
-     * Generates a response for an internal server error.
-     *
-     * This method determines the type of response based on whether the request is an API or hook request.
-     * If it is, it returns a 500 response with the provided message. Otherwise, it either redirects back
-     * with an error message or to a specified URL with an error message.
-     *
-     * @param string $message The error message to be included in the response.
-     * @param string|null $forward_url The URL to redirect to if the request is not an API or hook request. Defaults to null.
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse A 500 response or a redirect response with an error message.
-     */
 
     /**
      * Generates an error response with a 500 status code.
      *
      * @param string $message The error message to be included in the response.
+     * @param array $errors An array of errors to include in the response. Default is an empty array.
      * @param string|null $forward_url Optional URL to redirect to.
      * @return string|Response|RedirectResponse The generated error response.
      */
-    public static function error(string $message, ?string $forward_url = null): string|JsonResponse|RedirectResponse
+    public static function error(string $message, array $errors = [], ?string $forward_url = null): string|JsonResponse|RedirectResponse
     {
-        return self::generateResponse(500, $message, $forward_url);
+        return self::generateResponse(
+            status: 500,
+            message: $message,
+            errors: $errors,
+            forward_url: $forward_url
+        );
     }
 
     /**
@@ -150,23 +176,27 @@ class ResponseHelper
      * @param string|null $forward_url Optional URL to forward to in case of an error.
      * @return string|Response|RedirectResponse The response corresponding to the exception code.
      */
-    public static function handleException(Exception $exception, ?string $forward_url = null): string|JsonResponse|RedirectResponse
+    public static function handleException(Exception $exception, array $errors = [], ?string $forward_url = null): string|JsonResponse|RedirectResponse
     {
         switch ($exception->getCode()) {
             case 400:
-                return self::badRequest($exception->getMessage(), $forward_url);
+                return self::badRequest($exception->getMessage(), $errors, $forward_url);
             case 401:
-                return self::unautorized($exception->getMessage(), $forward_url);
+                return self::unautorized($exception->getMessage(), $errors, $forward_url);
             case 403:
-                return self::forbidden($exception->getMessage(), $forward_url);
+                return self::forbidden($exception->getMessage(), $errors, $forward_url);
             case 404:
-                return self::notFound($exception->getMessage(), $forward_url);
+                return self::notFound($exception->getMessage(), $errors, $forward_url);
             case 409:
-                return self::conflict($exception->getMessage(), $forward_url);
+                return self::conflict($exception->getMessage(), $errors, $forward_url);
             case 500:
-                return self::error($exception->getMessage(), $forward_url);
+                return self::error($exception->getMessage(), $errors, $forward_url);
             default:
-                return self::error('An unexpected error occurred. (' . $exception->getCode() . '-' . $exception->getMessage() . ')', $forward_url);
+                return self::error(
+                    message: 'An unexpected error occurred. (' . $exception->getCode() . '-' . $exception->getMessage() . ')',
+                    errors: $errors,
+                    forward_url: $forward_url
+                );
         }
     }
 }
